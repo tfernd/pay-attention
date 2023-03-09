2e5997f1faf966306b6cac4e969d6abf7170fab1
Thales Fernandes <ThalesFDFernandes@gmai.com>
2023-03-09 14:57:43 +0100

    mem and softmax

--- pay_attention/utils/__init__.py
+++ pay_attention/utils/__init__.py
@@ -1,1 +1,6 @@
 from .clear_cuda import clear_cuda
+
+from .memory import available_memory, allocated_memory
+del memory
+
+from .softmax import softmax, softmax_memory
---
+++ pay_attention/utils/softmax.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+import math
+import numpy as np
+
+import torch
+from torch import Tensor
+
+
+def softmax(
+    x: Tensor,  # (..., C)
+    inplace: bool = False,
+) -> Tensor:# (..., C)
+    """Applies the softmax function to the input tensor `x` along the last dimension."""
+
+    if not inplace:
+        return x.softmax(dim=-1, dtype=x.dtype)
+
+    x -= x.amax(dim=-1, keepdim=True)
+    x.exp_()
+    x /= x.sum(dim=-1, keepdim=True)
+
+    return x
+
+
+def softmax_memory(
+    x: Tensor,  # (..., C)
+    inplace: bool = False,
+) -> int:
+    assert x.dtype in (torch.float32, torch.half)
+
+    *shape, C = x.shape
+    B = np.prod(shape).item()
+
+    element_size = 4 if x.dtype == torch.float32 else 2
+
+    if inplace:
+        min_size = 128 if x.dtype == torch.float32 else 256 # cache size? warp-size?
+        
+        B += min_size - B % min_size
+
+        return element_size * B
+
+    # magic number that takes into account non-power of 2 B's ans C's
+    MAGIC = 1.01
+
+    return math.ceil(element_size * B * C * MAGIC)
